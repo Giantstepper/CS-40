b9705cdb30b7eef6587c818d59ea44b5bdf672d5
Giantstepper <emilycassell@WL-199-241.WPA.Claremont.Edu>
2015-09-21 23:11:22 -0700

    Init commit
    Original

---
+++ Simple.py
@@ -0,0 +1,9 @@
+3+4
+print 3+4
+print 4+4
+print 'if you are reading this the experiment has been a success'
+print 23*(12+54)
+print 3.4/2.3
+print 'Hello,'+'world!'
+type (2)
+
---
+++ ch2.py
@@ -0,0 +1,88 @@
+""" (a multiline comment)
+    This file contains some examples from
+    Chapter 2 of the textbook
+"""
+
+# 2.1 values and types
+print type('Hello, world!')
+print type(22)
+print type(34.5)
+print type('4.8')
+print 1000000    # a million
+print 1,000,000  # not a million but a tuple of three elements
+                 # don't worry about what a tuple is yet
+
+
+# 2.2 Variables
+fruit = 'apple'
+print fruit
+fruit = 'orange'
+print fruit
+
+# base 8, 16, and 2 numbers
+oct = 017
+print oct
+hex = 0xa
+print hex
+bin = 0b11
+print bin
+ten = 11   # 11 in base 10 number
+print ten
+
+
+# 2.3 Variable names and keywords
+
+# Some are illegal to use
+
+## 49ers = 'SF Football' Variable name cannot start with a number
+## print 49ers
+fniners = 'SF Football'
+print fniners
+
+## class = 'An illegal name to use'  # class is one of the keywords in Python
+## print class
+
+
+# 2.4 Operators and operands
+print 'Section 2.4'
+a = 2
+b = 3
+print a**b
+
+a = 1
+b = 1
+print a^b  # ^ is bitwise XOR
+
+
+minutes = 69
+print minutes/60
+
+print minutes/60.0
+"""
+
+
+# 2.5 Expressions and statements
+# Read the section and try examples
+
+
+# 2.6 Interactive mode and script mode
+# Read the section and try examples
+
+
+# do Exercise 2.2
+
+
+# 2.7 Order of operations
+# Read the section and try examples
+
+
+# 2.8 String operations
+
+fruit = 'apple'
+pie = ' pie yum yum'
+print fruit + pie
+
+
+# 2.10 Debugging
+# Read the section and try examples
+"""
